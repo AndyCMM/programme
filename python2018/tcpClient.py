03d0bbfe3b9aea94bb2b2d9afd5955e9da0ea8d5
AndyCMM <953087547@qq.com>
2019-03-21 20:58:47 +0800

    tcpserver,client

--- python2018/tcpClient.py
+++ python2018/tcpClient.py
@@ -2,15 +2,18 @@ import socket
 
 
 def main():
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ip = input('please input IP:')
-    port = int(input('please input port:'))
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # ip = input('please input IP:')
+    # port = int(input('please input port:'))
     # tcp.bind(('localhost', 8989))
-    ip = ''
-    port = 8989
+    ip = 'localhost'
+    port = 8991
+    tcp.bind(('', 9001))
     tcp.connect((ip, port))
     send_data = input(':')
     tcp.send(send_data.encode())
+    data = tcp.recv(1024)
+    print(data)
     tcp.close()
 
 
